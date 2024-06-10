2726ed74dbe02bbd923a8bf5003089d0d2d7a4e5
Ivan Libonatti <kessler1459@gmail.com>
2024-06-09 21:14:04 -0300

    ups

--- scrappers/scrapper.py
+++ scrappers/scrapper.py
@@ -42,7 +42,6 @@ class Scrapper(ABC):
             return res
         cli = AsyncClient(
             headers={
-                "kavak-country-acronym": "ar",
                 "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
             }
         )
